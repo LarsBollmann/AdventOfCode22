5b9d5a874c7534b5935d04982a720b117441b741
Lars Bollmann <lars.bollmann@tu-dortmund.de>
2022-12-11 21:03:09 +0100

    9 and 10 done

---
+++ src/day10.rs
@@ -0,0 +1,115 @@
+mod input;
+
+struct Cpu {
+    register: i32,
+    cycle: i32,
+    history: Vec<i32>,
+    output: String,
+    width: usize,
+}
+
+enum Instruction {
+    Addx(i32),
+    Noop
+}
+
+impl Cpu {
+    fn new() -> Self {
+        Cpu {
+            register: 1,
+            cycle: 0,
+            history: Vec::new(),
+            output: String::new(),
+            width: 40,
+        }
+    }
+
+    fn advance_cycle(&mut self) {
+        let x = self.cycle % self.width as i32;
+        if x == 0 && self.cycle != 0 {
+            self.output.push('\n');
+        }
+
+        if (self.register - x).abs() <= 1 {
+            self.output.push('#');
+        } else {
+            self.output.push('.');
+        }
+
+        self.history.push(self.register);
+        self.cycle += 1;
+    }
+
+    fn execute(&mut self, instruction: Instruction) {
+        match instruction {
+            Instruction::Addx(x) => {
+                self.advance_cycle();
+                self.advance_cycle();
+                self.register += x
+            },
+            Instruction::Noop => self.advance_cycle(),
+        }
+    }
+}
+
+fn parse_input(input: &str) -> Vec<Instruction> {
+    input.lines().map(|line| {
+        let parts = line.split_whitespace().collect::<Vec<&str>>();
+        match parts[0] {
+            "addx" => Instruction::Addx(parts[1].parse::<i32>().unwrap()),
+            "noop" => Instruction::Noop,
+            _ => panic!("Unknown instruction"),
+        }
+    }).collect()
+}
+fn main() {
+    let inp = input::get_input(10);
+    println!("Part 1: {}", part_1(parse_input(&inp)));
+    println!("Part 2:\n{}", part_2(parse_input(&inp)));
+}
+
+fn part_1(instructions: Vec<Instruction>) -> i32 {
+    let mut cpu = Cpu::new();
+    for instruction in instructions {
+        cpu.execute(instruction);
+    }
+    cpu.history.iter().skip(19).step_by(40).enumerate().map(|(i, x)| {
+        (i*40+20) as i32 * x
+    }).sum()
+}
+
+fn part_2(instructions: Vec<Instruction>) -> String {
+    let mut cpu = Cpu::new();
+
+    for instruction in instructions {
+        cpu.execute(instruction);
+    }
+    cpu.output
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let inp = fs::read_to_string("src/day10_test_input.txt").unwrap();
+        let inp = parse_input(&inp);
+        assert_eq!(part_1(inp), 13140);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let inp = fs::read_to_string("src/day10_test_input.txt").unwrap();
+        let inp = parse_input(&inp);
+        assert_eq!(part_2(inp), 
+"##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....");
+    }
+}
--- src/day9.rs
+++ src/day9.rs
@@ -1,145 +1,161 @@
-use std::{fmt::{Display, Error, Formatter}, collections::HashMap};
+use std::{
+    collections::HashMap,
+    fmt::{Display, Error, Formatter},
+};
 
 mod input;
 
-type Position = (i32, i32);
+type Position = [i32; 2];
 
 struct Rope {
-    head: Position,
-    tail: Position,
+    knots: Vec<Position>,
     visited_positions: HashMap<Position, bool>,
 }
 
 impl Rope {
-    fn new() -> Self {
+    fn with_lenght(length: usize) -> Self {
         Rope {
-            head: (0, 0),
-            tail: (0, 0),
+            knots: vec![[0, 0]; length],
             visited_positions: HashMap::new(),
         }
     }
 
-    fn move_up(&mut self) {
-        self.head.1 += 1;
-        let distance = self.get_distance();
+    fn adjust_tail(&mut self, head_index: usize, tail_index: usize) {
+        let distance = self.get_distance(head_index, tail_index);
+        let head = self.knots[head_index];
+        let tail = &mut self.knots[tail_index];
         if distance == 2.0 {
-            self.tail.1 += 1;
-        } else if distance > 2.0 {
-            if self.tail.0 < self.head.0 {
-                self.tail.0 += 1;
-                self.tail.1 += 1;
-            } else {
-                self.tail.0 -= 1;
-                self.tail.1 += 1;
+            tail[0] += (head[0] - tail[0]) / 2;
+            tail[1] += (head[1] - tail[1]) / 2;
+        }
+        if distance > 2.0 {
+            if head[0] - 1 > tail[0] && head[1] > tail[1] {
+                tail[0] += 1;
+                tail[1] += 1;
+            } else if head[0] - 1 > tail[0] && head[1] < tail[1] {
+                tail[0] += 1;
+                tail[1] -= 1;
+            } else if head[0] + 1 < tail[0] && head[1] > tail[1] {
+                tail[0] -= 1;
+                tail[1] += 1;
+            } else if head[0] + 1 < tail[0] && head[1] < tail[1] {
+                tail[0] -= 1;
+                tail[1] -= 1;
+            } else if head[0] > tail[0] && head[1] - 1 > tail[1] {
+                tail[0] += 1;
+                tail[1] += 1;
+            } else if head[0] > tail[0] && head[1] + 1 < tail[1] {
+                tail[0] += 1;
+                tail[1] -= 1;
+            } else if head[0] < tail[0] && head[1] - 1 > tail[1] {
+                tail[0] -= 1;
+                tail[1] += 1;
+            } else if head[0] < tail[0] && head[1] + 1 < tail[1] {
+                tail[0] -= 1;
+                tail[1] -= 1;
             }
         }
-        self.visited_positions.insert(self.tail, true);
+    }
+
+    fn move_direction(&mut self, direction: &str) {
+        match direction {
+            "U" => {
+                self.move_up();
+            }
+            "D" => {
+                self.move_down();
+            }
+            "L" => {
+                self.move_left();
+            }
+            "R" => {
+                self.move_right();
+            }
+            _ => panic!("Unknown direction"),
+        }
+    }
+
+    fn move_up(&mut self) {
+        self.knots[0][1] += 1;
+        for i in 1..self.knots.len() {
+            self.adjust_tail(i-1, i);
+        }
+        self.visited_positions
+            .insert(self.knots.last().unwrap().to_owned(), true);
     }
 
     fn move_down(&mut self) {
-        self.head.1 -= 1;
-        let distance = self.get_distance();
-        if distance == 2.0 {
-            self.tail.1 -= 1;
-        } else if distance > 2.0 {
-            if self.tail.0 < self.head.0 {
-                self.tail.0 += 1;
-                self.tail.1 -= 1;
-            } else {
-                self.tail.0 -= 1;
-                self.tail.1 -= 1;
-            }
+        self.knots[0][1] -= 1;
+        for i in 1..self.knots.len() {
+            self.adjust_tail(i-1, i);
         }
-        self.visited_positions.insert(self.tail, true);
+        self.visited_positions
+            .insert(self.knots.last().unwrap().to_owned(), true);
     }
 
     fn move_left(&mut self) {
-        self.head.0 -= 1;
-        let distance = self.get_distance();
-        if distance == 2.0 {
-            self.tail.0 -= 1;
-        } else if distance > 2.0 {
-            if self.tail.1 < self.head.1 {
-                self.tail.0 -= 1;
-                self.tail.1 += 1;
-            } else {
-                self.tail.0 -= 1;
-                self.tail.1 -= 1;
-            }
+        self.knots[0][0] -= 1;
+        for i in 1..self.knots.len() {
+            self.adjust_tail(i-1, i);
         }
-        self.visited_positions.insert(self.tail, true);
+        self.visited_positions
+            .insert(self.knots.last().unwrap().to_owned(), true);
     }
 
     fn move_right(&mut self) {
-        self.head.0 += 1;
-        let distance = self.get_distance();
-        if distance == 2.0 {
-            self.tail.0 += 1;
-        } else if distance > 2.0 {
-            if self.tail.1 < self.head.1 {
-                self.tail.0 += 1;
-                self.tail.1 += 1;
-            } else {
-                self.tail.0 += 1;
-                self.tail.1 -= 1;
-            }
+        self.knots[0][0] += 1;
+        for i in 1..self.knots.len() {
+            self.adjust_tail(i-1, i);
         }
-        self.visited_positions.insert(self.tail, true);
+        self.visited_positions
+            .insert(self.knots.last().unwrap().to_owned(), true);
     }
 
-    fn get_distance(&self) -> f64 {
+    fn get_distance(&self, head: usize, tail: usize) -> f64 {
         f64::sqrt(
-            f64::powi(self.head.0 as f64 - self.tail.0 as f64, 2)
-                + f64::powi(self.head.1 as f64 - self.tail.1 as f64, 2),
+            ((self.knots[head][0] - self.knots[tail][0]).pow(2)
+                + (self.knots[head][1] - self.knots[tail][1]).pow(2)) as f64,
         )
     }
-
-    
 }
 
 impl Display for Rope {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        write!(
-            f,
-            "({}, {}) -> ({}, {})",
-            self.head.0, self.head.1, self.tail.0, self.tail.1
-        )
+        for knot in &self.knots {
+            write!(f, "({}, {})", knot[0], knot[1])?;
+        }
+        Ok(())
     }
 }
 
 fn main() {
     let inp = input::get_input(9);
     println!("Part 1: {}", part_1(&inp));
+    println!("Part 2: {}", part_2(&inp));
 }
 
 fn part_1(input: &str) -> usize {
-    let mut rope = Rope::new();
+    let mut rope = Rope::with_lenght(2);
 
     for instruction in input.lines() {
         let (direction, steps) = instruction.split_at(1);
         let steps = steps.trim().parse::<i32>().unwrap();
-        match direction {
-            "U" => {
-                for _ in 0..steps {
-                    rope.move_up();
-                }
-            }
-            "D" => {
-                for _ in 0..steps {
-                    rope.move_down();
-                }
-            }
-            "L" => {
-                for _ in 0..steps {
-                    rope.move_left();
-                }
-            }
-            "R" => {
-                for _ in 0..steps {
-                    rope.move_right();
-                }
-            }
-            _ => panic!("Unknown direction"),
+        for _ in 0..steps {
+            rope.move_direction(direction);
+        }
+    }
+
+    rope.visited_positions.len()
+}
+
+fn part_2(input: &str) -> usize {
+    let mut rope = Rope::with_lenght(10);
+
+    for instruction in input.lines() {
+        let (direction, steps) = instruction.split_at(1);
+        let steps = steps.trim().parse::<i32>().unwrap();
+        for _ in 0..steps {
+            rope.move_direction(direction);
         }
     }
 
@@ -158,8 +174,23 @@ D 1
 L 5
 R 2";
 
+    const INPUT2: &str = "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20";
+
     #[test]
     fn test_part1() {
         assert_eq!(part_1(INPUT), 13);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part_2(INPUT), 1);
+        assert_eq!(part_2(INPUT2), 36);
     }
 }
