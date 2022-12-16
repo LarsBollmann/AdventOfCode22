7ddac82e11d5406e3599734082f01cd76f03c65c
Lars Bollmann <55883538+LarsBollmann@users.noreply.github.com>
2022-12-16 21:54:17 +0000

    Day 16 done (more or less)

---
+++ src/day16.rs
@@ -0,0 +1,264 @@
+mod input;
+
+use std::{collections::{BinaryHeap, HashMap, HashSet}, hash::Hash};
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+struct Valve {
+    flow_rate: usize,
+    connects_to: Vec<(String, usize)>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct QueueItem<'a> {
+    valve: &'a Valve,
+    cost: usize,
+}
+
+impl Ord for QueueItem<'_> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.cost.cmp(&self.cost)
+    }
+}
+
+impl PartialOrd for QueueItem<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(other.cost.cmp(&self.cost))
+    }
+}
+
+fn main() {
+    let inp = input::get_input(16);
+    let valves = parse_input(&inp);
+    //println!("Valves: {:?}", valves);
+    println!("Part 1: {}", part_1(valves.clone()));
+    println!("Part 2: {}", part_2(valves.clone()));
+}
+
+fn compress_graph(valves: HashMap<&str, Valve>) -> HashMap<&str, Valve> {
+    let mut new_valves = HashMap::new();
+    let start_iter = valves.iter().filter(|(name, valve)| valve.flow_rate != 0 || **name == "AA"); 
+    let end_iter = valves.iter().filter(|(_, valve)| valve.flow_rate != 0);
+    for (start_name, start_valve) in start_iter {
+        let mut new_connects_to = Vec::new();
+        for (end_name, _) in end_iter.clone() {
+            new_connects_to.push((end_name.to_string(), get_fastest_connection(&valves, start_name, end_name).unwrap()));
+        }
+        new_valves.insert(*start_name, Valve {
+            flow_rate: start_valve.flow_rate,
+            connects_to: new_connects_to,
+        });
+    }
+
+    new_valves
+}
+
+fn parse_input(inp: &str) -> HashMap<&str, Valve> {
+    let mut valves = HashMap::new();
+
+    for line in inp.lines() {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        let name = parts[1];
+        let flow_rate: usize = parts[4]
+            .trim_matches(|c: char| !c.is_ascii_digit())
+            .parse()
+            .unwrap();
+        let connects_to = parts[9..]
+            .iter()
+            .map(|s| (s.trim_end_matches(',').to_string(), 1))
+            .collect();
+        valves.insert(
+            name,
+            Valve {
+                flow_rate,
+                connects_to,
+            },
+        );
+    }
+
+    compress_graph(valves)
+}
+
+fn get_fastest_connection(
+    valves: &HashMap<&str, Valve>,
+    start_valve: &str,
+    end_valve: &str,
+) -> Option<usize> {
+    let start_valve = valves.get(start_valve).unwrap();
+    let end_valve = valves.get(end_valve).unwrap();
+
+    let mut queue = BinaryHeap::new();
+    let mut visited = HashSet::new();
+
+    queue.push(QueueItem {
+        valve: start_valve,
+        cost: 0,
+    });
+
+    while let Some(QueueItem { valve, cost }) = queue.pop() {
+        if valve == end_valve {
+            return Some(cost);
+        }
+
+        let neighbours = valve.connects_to.iter().collect::<Vec<_>>();
+        for neighbour in neighbours {
+            if !visited.contains(neighbour.0.as_str()) {
+                visited.insert(neighbour.0.as_str());
+                queue.push(QueueItem {
+                    valve: valves.get(neighbour.0.as_str()).unwrap(),
+                    cost: cost + neighbour.1,
+                });
+            }
+        }
+    }
+    None
+}
+
+fn calculate_all_possible_paths(
+    valves: &HashMap<&str, Valve>,
+    start_valve: &str,
+    opened: HashSet<&str>,
+    minutes_left: usize,
+) -> usize {
+    let possible_connections = valves.get(start_valve).unwrap().connects_to
+        .iter()
+        .filter(|(end_valve, _)| !opened.contains(end_valve.as_str()))
+        .filter(|(_, fastest_connection)| *fastest_connection < minutes_left)
+        .collect::<Vec<_>>();
+
+        
+
+    let mut pressure_released_this_step = 0;
+    if opened.contains(start_valve) {
+        pressure_released_this_step = valves.get(start_valve).unwrap().flow_rate * minutes_left;
+    }
+
+    if possible_connections.is_empty() {
+        return pressure_released_this_step;
+    }
+
+    let mut max = 0;
+
+    for (end_valve, fastest_connection) in possible_connections {
+        let mut new_opened = opened.clone();
+        new_opened.insert(end_valve);
+        let score = calculate_all_possible_paths(
+            valves,
+            end_valve,
+            new_opened,
+            minutes_left - fastest_connection - 1,
+        );
+        if score > max {
+            max = score;
+        }
+    }
+    
+    max + pressure_released_this_step
+}
+
+fn calculate_all_possible_paths_2<'a>(
+    valves: &'a HashMap<&'a str, Valve>,
+    start_valve: &'a str,
+    opened: HashSet<&'a str>,
+    minutes_left: usize,
+    max_iterations: usize,
+) -> (usize, HashSet<&'a str>) {
+    let possible_connections = valves.get(start_valve).unwrap().connects_to
+        .iter()
+        .filter(|(end_valve, _)| !opened.contains(end_valve.as_str()))
+        .filter(|(_, fastest_connection)| *fastest_connection < minutes_left)
+        .collect::<Vec<_>>();
+
+
+
+
+    let mut pressure_released_this_step = 0;
+    if opened.contains(start_valve) {
+        pressure_released_this_step = valves.get(start_valve).unwrap().flow_rate * minutes_left;
+    }
+    //println!("{} {} {:?} {} {}", start_valve, minutes_left, opened, opened.len(), pressure_released_this_step);
+
+    if opened.len() >= max_iterations {
+        return (pressure_released_this_step, opened);
+    }
+
+    if possible_connections.is_empty() {
+        return (pressure_released_this_step, opened);
+    }
+
+    let mut max = 0;
+    let mut max_opened = HashSet::new();
+
+    for (end_valve, fastest_connection) in possible_connections {
+        let mut new_opened = opened.clone();
+        new_opened.insert(end_valve);
+        let (score, returned_opened) = calculate_all_possible_paths_2(
+            valves,
+            end_valve,
+            new_opened,
+            minutes_left - fastest_connection - 1,
+            max_iterations,
+        );
+        if score > max {
+            max = score;
+            max_opened = returned_opened;
+        }
+    }
+    
+    (max + pressure_released_this_step, max_opened)
+}
+
+fn part_1(valves: HashMap<&str, Valve>) -> usize {
+    let start_valve = "AA";
+    let minutes_left = 30;
+
+    let opened = HashSet::new();
+    calculate_all_possible_paths(&valves, start_valve, opened, minutes_left)
+}
+
+// As far as i know this works completely by accident. I could imagine cases where finding two independent 
+// paths actually would not work. If the calculated optimal first path includes a valve that would need
+// to be opened in the second path for maximum efficiency for example. 
+// Either:
+// 1. I am wrong and this actually works in all cases
+// 2. I am right and my input specifically happens to be one of the cases where this works
+// 3. I am right and the generation algorithm for the inputs somehow makes sure that this works
+// But since i have only my own input I can't really tell.
+// Anyway, spent way too much time on this so already so... Yeah.
+fn part_2(valves: HashMap<&str, Valve>) -> usize {
+    let mut max = 0;
+    for i in 2..9 {
+        let (pressure, opened) = calculate_all_possible_paths_2(&valves, "AA", HashSet::new(), 26, i);
+        let pressure_2 = calculate_all_possible_paths(&valves, "AA", opened, 26);
+        let total = pressure + pressure_2;
+        if total > max {
+            max = total;
+        }
+    }
+    max
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const INPUT: &str = "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II";
+
+    #[test]
+    fn test_part1() {
+        let parse_input = parse_input(INPUT);
+        assert_eq!(part_1(parse_input), 1651);
+    }
+
+    #[test]
+    fn test_part2() {
+        unimplemented!();
+    }
+}
